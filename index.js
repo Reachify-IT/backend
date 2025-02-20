6876a5c1859e06ea3a760b597bea59b7e21b380f
AnshulDogra001 <abhinav.dogra001@gmail.com>
2025-02-20 13:49:26 +0530

    comit

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const app = express();
 // Middlewares
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174", "http://13.126.156.121"],
+    origin: ["http://localhost:5173", "http://localhost:5174", "http://13.126.156.121", "http://13.126.156.121:80"],
     methods: "GET,POST,PUT,DELETE",
     allowedHeaders: "Content-Type,Authorization",
     credentials: true,
