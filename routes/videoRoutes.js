38fd58ec62fa9466ec0294bd28a146a3245583f0
AnshulDogra001 <abhinav.dogra001@gmail.com>
2025-03-11 16:27:16 +0530

    out

--- routes/videoRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { mergeVideos, getVideo } = require("../controllers/videoController");
-const router = express.Router();
-
-router.post("/merge", mergeVideos);
-router.get("/video/:id", getVideo);
-
-module.exports = router;
