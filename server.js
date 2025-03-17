db3a37f6f2088809526f62f1e6348d6e4852c22d
AnshulDogra001 <abhinav.dogra001@gmail.com>
2025-03-17 16:21:28 +0530

    final

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ const startServer = () => {
     .then(() => {
       const io = new Server(server, {
         cors: {
-          origin: ["http://localhost:5173", "http://localhost:5174", "http://frontend.reachifyinnovations.in"],
+          origin: ["http://loomifyinnovations.com"],
           allowedHeaders: ["Content-Type", "Authorization"],
           credentials: true,
         },
