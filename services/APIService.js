5a4bd9daf4ff89f7f919d198fb03b58e0a962830
AnshulDogra001 <abhinav.dogra001@gmail.com>
2025-03-20 16:57:21 +0530

    final

--- services/APIService.js
+++ services/APIService.js
@@ -43,7 +43,6 @@ const processEmailService = async ({
                 "Content-Type": "application/json",
                 "Accept": "application/json"
             },
-            timeout: 20000 // Set timeout to avoid indefinite hanging
         });
 
         console.log('Email processed successfully:', response.data);
