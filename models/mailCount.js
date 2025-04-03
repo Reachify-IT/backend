2ad89c547f6779b9e97cdbfa9aed6e4abc1bd123
AnshulDogra001 <abhinav.dogra001@gmail.com>
2025-04-03 14:19:51 +0530

    final

--- models/mailCount.js
+++ models/mailCount.js
@@ -1,16 +1,27 @@
 const mongoose = require("mongoose");
 
-const mailCountSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  successMails: {
-    type: Number,
-    default: 0, // Default count is zero
+const mailCountSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      unique: true, // Ensures one record per user
+    },
+    successMails: {
+      type: Number,
+      default: 0,
+    },
+    failedMails: {
+      type: Number,
+      default: 0,
+    },
   },
-  failedMails: {
-    type: Number,
-    default: 0, 
-  },
-}, { timestamps: true });
+  { 
+    timestamps: true, 
+    versionKey: false // Optional: removes __v field
+  }
+);
 
 const MailCount = mongoose.model("MailCount", mailCountSchema);
 
